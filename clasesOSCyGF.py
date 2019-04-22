f2ef687ef392530100f56b1ae677210e1e53fd45
Gabriel Paciaroni <g_paciaroni@hotmail.com>
2019-04-22 00:03:09 -0300

    Agregué los scripts del 16-4, falta terminar las clases de OSCI y FGEN.

--- clasesOSCyGF.py
+++ clasesOSCyGF.py
@@ -7,32 +7,33 @@ Created on Tue Apr  9 18:32:07 2019
 
 import visa
 import numpy as np
-from matplotlib import pyplot as pp
+from matplotlib import pyplot as plot
 
-rm = visa.ResourceManager()
+
 
 class OsciloscopeTDS1002B():
       
     def __init__(self, serial):
+        self.rm = visa.ResourceManager()        
         self.serial = serial
-        self.osci = rm.open_resource('USB0::0x0699::0x0363::{}::INSTR'.format(serial))
-        self.acquire = osci.write('ACQ:STATE 1')
+        self.osci = self.rm.open_resource('USB0::0x0699::0x0363::{}::INSTR'.format(serial))
+        self.acquire = self.osci.write('ACQ:STATE 1')
     
     def idn(self):
         return self.osci.query('*IDN?')
         
     def data_points(self):
-        YMU = float(osci.query('WFMP:YMU?'))
-        YOF = float(osci.query('WFMP:YOF?'))
-        YZE = float(osci.query('WFMP:YZERO?'))
-        yn_dl= osci.query_binary_values('CURV?',datatype='B', is_big_endian=True)
+        YMU = float(self.osci.query('WFMP:YMU?'))
+        YOF = float(self.osci.query('WFMP:YOF?'))
+        YZE = float(self.osci.query('WFMP:YZERO?'))
+        yn_dl = self.osci.query_binary_values('CURV?',datatype='B', is_big_endian=True)
         
         Yn = YZE + YMU*(np.array(yn_dl) - YOF)
 
-        XIN=float(osci.query('WFMP:XIN?'))
-        #XUN = osci.query('WFMP:XUN?')
-        XZE = float(osci.query('WFMP:XZE?'))
-        PT_OF = float(osci.query('WFMP:PT_OF?'))
+        XIN=float(self.osci.query('WFMP:XIN?'))
+        #XUN = self.osci.query('WFMP:XUN?')
+        XZE = float(self.osci.query('WFMP:XZE?'))
+        PT_OF = float(self.osci.query('WFMP:PT_OF?'))
 
         Xn = XZE + XIN*(np.linspace(1,2500,2500) - PT_OF)
         return (Xn,Yn)    
@@ -40,15 +41,39 @@ class OsciloscopeTDS1002B():
     def set_yscale(self, yscale, channel = 1):
         self.osci.write('CH'+str(channel)+':SCA'+str(yscale))
         
-    def read_yscale(self, channel = 1):
+    def get_yscale(self, channel = 1):
         return self.osci.query('CH'+str(channel)+':SCA?')
     
     def set_xscale(self, xscale):
         self.osci.write('HOR:DEL:SCA'+ str(xscale))
-
-OsciloscopeTDS1002B.osci
-
-#oscil = OsciloscopeTDS1002B('C102220')
+        
+    def get_freq(self):
+        self.osci.query()
+        
+class FunctionGeneratorAFG3021B():
+    
+    def __init__(self, serial):
+        self.rm = visa.ResourceManager()        
+        self.serial = serial
+        self.fgen = self.rm.open_resource('USB0::0x0699::0x0346::{}::INSTR'.format(serial))
+        #self.fgen.turn_on = self.fgen.write('OUTP1:STAT 1')
+        
+    def set_amp(self, amp):
+        self.fgen.write('SOUR1:VOLT:LEV:IMM:AMPL '+str(amp)+'VPP')
+        
+    def set_freq(self, freq):
+        self.fgen.write('SOUR1:FREQ {}'.format(freq))
+        
+    def get_freq(self):
+        return self.fgen.query('SOUR1:FREQ?') 
+        
+    def set_offset(self, offset):
+        self.fgen.write('SOUR1:VOLT:OFFSET {}'.format(offset))
+    
+    
+    
+osc = OsciloscopeTDS1002B('C065089')
+#OsciloscopeTDS1002B.osci
 #oscil.idn()
 #oscil.read_yscale(1)
 #datos = oscil.data_points()
