27a854da51853bf2fb3f739b975b43545ff78b8f
tomas <tomygiudice@gmail.com>
2019-06-28 20:02:51 -0300

    viernes 28-6

--- ESPECIAL/sketch_jun25a_servoControl_prueba/sketch_jun25a_servoControl/sketch_jun25a_servoControl.ino
+++ ESPECIAL/sketch_jun25a_servoControl_prueba/sketch_jun25a_servoControl/sketch_jun25a_servoControl.ino
@@ -6,22 +6,37 @@
  by Scott Fitzgerald
  http://www.arduino.cc/en/Tutorial/Knob
 */
-
+#include <Arduino.h>
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
-//int potpin= 0;  // analog pin used to connect the potentiometer
-//int val;    // variable to read the value from the analog pin
+
+int pos;
+int v=0;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
+
 void loop() {
+myservo.writeMicroseconds(650);
+delay(100);
+//myservo.writeMicroseconds(1500);
+//myservo.write(90);
+
+}
+
+
+
+
+
+// void loop() {
 //  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
 
 //  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(20);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
-}
+                   // sets the servo position according to the scaled value
+  //myservo.write(5);  
+  //delay(15);                           // waits for the servo to get there
+ 
