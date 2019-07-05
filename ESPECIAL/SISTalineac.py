04e6d59eb025df530716e8c0f3dbbd0b8194f5ac
ethan <tomygiudice@gmail.com>
2019-07-05 05:47:45 -0300

    script principal del sist de alineacion es SISTalineac.py

---
+++ ESPECIAL/SISTalineac.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 03:53:58 2019
+
+@author: KBZ
+"""
+
+from matplotlib import pyplot as pp
+import numpy as np
+import cv2 
+import lantz
+import time
+from lantz.ino import INODriver, QuantityFeat, BoolFeat
+from Arduino0 import Servo
+
+#input del usuario
+musi=input('posic inicial del servo:')
+v=eval(input('vector de barrido en microseconds del ciclo PWM del servo (puede usarse np.linspace por ej):'))
+
+musi=float(musi)#posic inic...conviene trabajar en rangos cercanos a los 1500 microseconds
+
+camera = cv2.VideoCapture(0)  
+
+if __name__ == '__main__':
+    with Servo.via_packfile('Servo.pack.yaml') as dev:
+        dev.angle = musi # Posicionamiento inicial 
+
+
+poss=[] #repositorio
+
+    
+if __name__ == '__main__':
+    with Servo.via_packfile('Servo.pack.yaml') as dev:
+        for i in v:
+            print('posicion ',i)
+            dev.angle = i #orden de posicionamiento al servo/arduino
+            time.sleep(20) #espera de correccion a la posicion
+            return_value, img = camera.read() #tomo la foto
+            nombre='imagenes/foto{}.png'.format(np.around(i,decimals=2))
+            cv2.imwrite(nombre,img) #guardo la foto
+            poss.append([i,nombre]) #guardo ubicaciones de cada foto
+
+del(camera) #libero la camara
+
+poss=np.array(poss)
+
+#ANALISIS DE LAS FOTOS
+
+def func(x, m, x0, y0): #funcion lineal para cuadrados minimos
+    return m*(x-x0)+y0
+
+ind=0
+rs=[] #ratios de varianzas varx/vary
+
+for f in poss[:,1]:
+    foto=cv2.imread('imagenes/'+f)
+    foto=np.array(foto)
+    fotoPROM=np.sum(foto,axis=2)/3#promedio de las matrices RGB
+    lim=np.mean(fotoPROM) #promedio absoluto de intensidad
+    val,binary=cv2.threshold(fotoPROM,lim,255,cv2.THRESH_BINARY) #binarización de la imagen
+    y,x=np.nonzero(binary) #posiciones de las iluminaciones
+    y=480-y #redefinicion de origen
+    params,covar=curve_fit(func,x,y,p0=(0.3,0,200))# cuadrados minimos para sacar el eje principal de la difraccion , p0 parametros iniciales
+    dire=[5,func(5,params[0],params[1],params[2])-func(0,params[0],params[1],params[2])] #vector director del eje principal  del patron
+    rads=np.arctan(-dire[1]/dire[0]) #angulo de rotación para enderezar el patron respecto al eje de la camara
+    [rows,cols]=fotoPROM.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),rads*180/np.pi,1) #construyo la matriz de rotación
+    dst = cv2.warpAffine(fotoPROM,M,(cols,rows)) #enderezo la imagen
+    proyY=np.sum(dst,axis=0) #vector de proyeccion de intensidades en eje Y
+    if np.sum(proyY)==0: proyY=1 #evito un error de division por 0
+    proyX=np.sum(dst,axis=1) #vector de proyeccion de intensidades en eje X
+    r=np.var(proyX)/np.var(proyY) #la razon de varianzas de las distribuciones de intensidad en x respecto de y
+    rs=rs.append([poss[ind,0],r])
+    ind=ind+1
+
+rs=np.array(rs)
+iM=np.argmax(rs[:,1])
+posicM=rs[:,0][iM]
+
+if __name__ == '__main__':
+    with Servo.via_packfile('Servo.pack.yaml') as dev:
+        dev.angle = posicM # Posicionamiento final en donde hay difraccion 
+
--- ESPECIAL/analImag.py
+++ ESPECIAL/analImag.py
@@ -99,3 +99,137 @@ axs[0].imshow(fotoPROM)
 axs[0].plot(xx,fvals2,'r.')
 axs[1].plot(intens,'-')
 
+#ANALISIS POR VARIANCIA (PROPUESTA por HERNAN GRECCO)
+
+#1er aproximacion simple comparacion de varianza segun ejes de la camara
+
+proyY=np.sum(fotoPROM,axis=0)
+proyX=np.sum(fotoPROM,axis=1)
+
+plt.plot(proyY)
+plt.plot(proyX)
+
+rxy=np.var(proyX)/np.var(proyY)
+
+#################################################
+#2da aproximacion : comparacion de varianza segun ejes de la figura de difraccion (generalizacion para cualquier orientacion de rendija)
+
+            #spot=cv2.imread('spot.png')
+            #
+            #spot=np.array(spot)
+            #spotPROM=np.sum(spot,axis=2)/3
+            #
+            #lim=np.mean(spotPROM)
+            #val,binaryspot=cv2.threshold(spotPROM,lim,255,cv2.THRESH_BINARY)
+            #
+            #proyX=np.sum(binaryspot,axis=0)/480
+            #proyY=np.sum(binaryspot,axis=1)/640
+            #
+            #j=0
+            #v=0
+            #for i in proyY:
+            #    v=i*j+v
+            #    j=j+1
+            #    
+            #yspot=v/np.sum(proyY)
+            #
+            #
+            #j=0
+            #v=0
+            #for i in proyX:
+            #    v=i*j+v
+            #    j=j+1
+            #    
+            #xspot=v/np.sum(proyX)
+            #
+            #plt.figure()    
+            #plt.imshow(binaryspot)
+            #plt.plot(xspot,yspot,'k.')
+
+
+            #y,x=np.nonzero(binaryspot)
+            #y=480-y
+
+
+
+        #def func(x, m, x0, y0):
+        #    return m*(x-x0)+y0
+        #
+        #params,covar=curve_fit(func,x,y,p0=(0.3,0,200))
+
+
+
+
+            ##verifico que el centro de masa del spot coincide con el punto del ajuste de cuad min para xCM func(xCM)
+            #plt.figure()    
+            #plt.plot(x,y,'.')
+            #plt.plot(xspot,func(xspot,params[0],params[1],params[2]),'g.')
+            #plt.xlim(0,640)
+            #plt.ylim(0,480)
+
+#saco la direccion perpendicular al eje del patron
+dire=[5,func(xspot+5,params[0],params[1],params[2])-yspot]
+
+        #perp=[dire[1],-dire[0]]
+        #
+        ##saco la pendiente
+        #m=-dire[0]/dire[1]
+        #
+        #
+        #xx=np.linspace(0,639,640)
+        #
+        #
+        ##funcion de la direccion perpendicular
+        #eje2=lambda x: m*(x-xspot)+yspot
+        #eje2vals=eje2(xx).astype(int)
+
+
+fvals=func(xx,params[0],params[1],params[2])
+fvals=fvals.astype(int)
+
+        #observamos las 2 direcciones y el punto central
+        #plt.figure()    
+        #plt.imshow(binaryspot)
+        #plt.plot(xx,eje2vals,'r-')
+        #plt.plot(xx,fvals,'b-')
+        #plt.plot(xspot,yspot,'k.')
+        #plt.xlim(0,640)
+        #plt.ylim(0,480)
+        #plt.plot(xspot,func(xspot,params[0],params[1],params[2]),'g.')
+
+#################################################
+#VERSION 3 rotacion de la imagen SIN conocer SPOT
+foto=cv2.imread('foto.png')
+
+
+plt.figure(1)    
+plt.plot(x,y,'.')
+plt.xlim(0,640)
+plt.ylim(0,480)
+
+def func(x, m, x0, y0):
+    return m*(x-x0)+y0
+
+params,covar=curve_fit(func,x,y,p0=(0.3,0,200))
+
+dire=[5,func(5,params[0],params[1],params[2])-func(0,params[0],params[1],params[2])]
+
+rads=np.arctan(-dire[1]/dire[0])
+
+[rows,cols]=fotoPROM.shape
+
+M = cv2.getRotationMatrix2D((cols/2,rows/2),rads*180/np.pi,1)
+dst = cv2.warpAffine(fotoPROM,M,(cols,rows))
+
+plt.imshow(dst) #Ahora la figura esta al derecho y se puede aplicar el criterio de la primer version
+
+
+proyY=np.sum(dst,axis=0)
+proyX=np.sum(dst,axis=1)
+
+plt.plot(proyY)
+plt.plot(proyX)
+
+rxy=np.var(proyX)/np.var(proyY) #la razon de varianzas de las distribuciones de intensidad en x respecto de y
+
+
